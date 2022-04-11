2221929c23a395f127272c8dc719dd25be6cdd32
RitterRen <renyaokun0907@gmail.com>
2022-04-11 13:11:10 -0700

    Binary Search: 212

--- src/BinarySearch/SingleElementInASortedArray.java
+++ src/BinarySearch/SingleElementInASortedArray.java
@@ -1,4 +1,4 @@
-package leetcode.editor.cn;
+package BinarySearch;
 
 //You are given a sorted array consisting of only integers where every element 
 //appears exactly twice, except for one element which appears exactly once. 
